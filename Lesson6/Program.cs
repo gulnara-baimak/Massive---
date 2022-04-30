56559abda46ca9e8236518847c94202de063cff7
Gulnara <gulnara@mail.rum>
2022-05-01 01:14:21 +0600

    Lesson6

---
+++ Lesson6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+Console.WriteLine($"Введите число: ");
+int n = Convert.ToInt32(System.Console.ReadLine());
+int s = 0;
+while (n > 0) 
+{
+    s = s + n % 10;
+    n = n /10 ;
+}
+ Console.WriteLine(s);
+ Console.WriteLine($"Сумма цифр в числе: {s}" );

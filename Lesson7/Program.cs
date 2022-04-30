482b89961d8ec21a8066aaa947e0569269a7fdaf
Gulnara <gulnara@mail.rum>
2022-05-01 01:14:35 +0600

    Lesson7

---
+++ Lesson7/Program.cs
@@ -0,0 +1,18 @@
+﻿// 6.	Задать массив, заполнить случайными положительными трёхзначными числами. 
+// Показать количество четных чисел
+
+double GetCount( int[] array)
+{
+    double count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+         if (array[i]%2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int[] arr = {113, 324, 457, 222, 997, 444, 100, 543};
+double result  = GetCount(arr);
+Console.WriteLine($"Количество четных чисел {result}");

9e8d52b508da898b643891f74284f86e79b5eb0b
Gulnara <gulnara@mail.rum>
2022-05-01 01:06:40 +0600

    Lesson1

---
+++ Lesson1/Program.cs
@@ -0,0 +1,29 @@
+﻿// 1. Задать массив из 8 элементов и вывести их на экран
+
+int[] Massive = new int[8];
+void FullArray(int[] Maks)
+{
+    int index = 0;
+    int length = Maks.Length;
+
+    while (index < length)
+    {
+        Maks[index] = new Random().Next(0, 10);
+        index ++;
+    }
+}
+void PrintArray(int[] Maks)
+{
+    int count = 0;
+    int lenght = Maks.Length;
+
+    while (count < lenght)
+    {
+        Console.WriteLine($"Элементы массива {Maks[count]}");
+        count++;
+    }
+
+}
+FullArray (Massive);
+PrintArray (Massive);
+
